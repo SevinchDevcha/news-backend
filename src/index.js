0a3d87c11fd810f8df0615702399826f04528d17
Sevinch <erkinovasevinch38@gmail.com>
2025-09-16 08:11:46 +0500

    loading

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ app.use(express.text())
 
 
 app.use(cors({
-  origin: "*"
+  origin: "*",
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
 }));
 
 
